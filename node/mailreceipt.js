bc5063838aa3a60c313410342a4511b2fae0a938
Mike Menne <mpmenne@essav.com>
2014-10-02 05:07:46 +0000

    adding in my email as a cc on receipts

--- node/mailreceipt.js
+++ node/mailreceipt.js
@@ -19,8 +19,9 @@ exports.sendEmail = function(email, orderNumber) {
   var mailOptions = {
     from: 'snapscrip+orders@gmail.com',
     to: email,
+    cc: 'snapscrip@gmail.com',
     subject: 'Order Received :-)',
-    html: '<html><body><h1>Thanks for your Order</h1><h3>Order #: ' + orderNumber  + '</h3><p>We faxed your order straight to St. Joan of Arc.  Typically it takes about a week for the gift cards to come in, but usually the more popular giftcards are kept on hand so parts of your order could be ready in the next day or two.</p><p>When the giftcards arrive, they can be picked up from the Rectory,  after Mass, or even sent home with your child from school</p></p><p>Also, remember if you paid with cash be sure to send a check with your child to school or drop one off at the rectory.</p><p>If you have any questions just send a note to snapscrip@gmail.com</p><p>Cheers!  Mike Menne (SnapScrip Founder) </p></body></html>'
+    html: '<html><body><h1>Thanks for your Order</h1><h3>Order #: ' + orderNumber  + '</h3><p>Your order was faxed straight to the St. Joan of Arc Rectory.  Typically it takes about a week for the gift cards to come in, but usually the more popular giftcards are kept on hand so parts of your order could be ready in the next day or two.</p><p>When the giftcards arrive, they can be picked up from the Rectory,  after Mass, or even sent home with your child from school</p></p><p>Also, remember if you paid with cash or check be sure to send a check with your child to school or drop one off at the rectory.</p><p>If you have any questions just send a note to snapscrip@gmail.com</p><p>Cheers!  Mike Menne \n St. Joan of Arc Parishioner and SnapScrip creator </p></body></html>'
   }
 
   transporter.sendMail(mailOptions, function(err, info) {
